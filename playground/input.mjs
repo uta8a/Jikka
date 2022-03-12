a18b48baf4f6c5b80c325f5bd058a0871d80615f
uta8a <uta8a@users.noreply.github.com>
2022-03-12 00:24:55 +0000

    deploy: 6a19dd8ee3a283f7164737520a0d70e5597d089d

---
+++ playground/input.mjs
@@ -0,0 +1,108 @@
+import * as rts from "./rts.mjs";
+import wasm from "./jikka-asterius.wasm.mjs";
+import req from "./jikka-asterius.req.mjs";
+
+async function convert(prog) {
+  const m = await wasm;
+  const i = await rts.newAsteriusInstance(Object.assign(req, { module: m }));
+  return await i.exports.convert(prog);
+}
+
+async function bundleRuntime(prog) {
+  const m = await wasm;
+  const i = await rts.newAsteriusInstance(Object.assign(req, { module: m }));
+  return await i.exports.bundleRuntime(prog);
+}
+
+async function embedOriginalCode(original, prog) {
+  const m = await wasm;
+  const i = await rts.newAsteriusInstance(Object.assign(req, { module: m }));
+  return await i.exports.embedOriginalCode(original, prog);
+}
+
+function loadData() {
+  const req = new XMLHttpRequest();
+  req.open("GET", "../gallery/data.json", false);
+  req.send();
+  if (req.status != 200) {
+    throw Error(req.statusText);
+  }
+  return JSON.parse(req.responseText);
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  require(["vs/editor/editor.main"], function () {
+    // make editors
+    const input = monaco.editor.create(document.getElementById("input"), {
+      value: "loading...",
+      language: "python",
+    });
+    const output = monaco.editor.create(document.getElementById("output"), {
+      value: "",
+      language: "cpp",
+    });
+
+    const bundle = document.getElementById("bundle");
+    const embed = document.getElementById("embed");
+
+    // transpiling periodically
+    let lastProgram = "";
+    let lastBundle = false;
+    let lastEmbed = false;
+    const sync = async function () {
+      try {
+        if (
+          input.getValue() != lastProgram ||
+          bundle.checked != lastBundle ||
+          embed.checked != lastEmbed
+        ) {
+          lastProgram = input.getValue();
+          lastBundle = bundle.checked;
+          lastEmbed = embed.checked;
+          output.setValue("transpiling...");
+          let transpiledProgram = await convert(lastProgram);
+          if (lastBundle) {
+            transpiledProgram = await bundleRuntime(transpiledProgram);
+          }
+          if (lastEmbed) {
+            transpiledProgram = await embedOriginalCode(
+              lastProgram,
+              transpiledProgram
+            );
+          }
+          output.setValue(transpiledProgram);
+        }
+      } catch (e) {
+        console.log(e);
+        output.setValue(e.toString());
+      }
+      setTimeout(sync, 1000);
+    };
+    sync();
+
+    // make dropdown menu of examples
+    const dropdown = document.getElementById("dropdown");
+    function addItem(row) {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.textContent = row["path"];
+      a.classList.add("dropdown-item");
+      a.addEventListener("click", function () {
+        input.setValue(row["python"]);
+      });
+      li.appendChild(a);
+      dropdown.appendChild(li);
+    }
+    const data = loadData();
+    for (const row of data["examples"]) {
+      addItem(row);
+      if (row["path"] == "examples/dp_z-kubaru.py") {
+        // default
+        input.setValue(row["python"]);
+      }
+    }
+    for (const row of data["errors"]) {
+      addItem(row);
+    }
+  });
+});
